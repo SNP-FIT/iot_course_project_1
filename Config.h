4369e777017e413850dd0feb26463a812aad527e
pepo4343 <ptangwongkit@gmail.com>
2025-12-03 23:25:54 +0700

    End of ep16

---
+++ Config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "BPSQUARE"
+#define WIFI_PASSWORD "WhoAreYou?"
+
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6dJhJB6Qf"
+#define BLYNK_TEMPLATE_NAME "IoT Project"
+#define BLYNK_AUTH_TOKEN "ImsB_HuzsnodSvzxTUBMG4un5YlkVRqu"
+
+#define RELAY_1_PIN 32
+#define RELAY_2_PIN 33
+
+#endif
--- iot_course_project_1.ino
+++ iot_course_project_1.ino
@@ -1,9 +1,33 @@
+#include "Config.h"
+#include <BlynkSimpleEsp32.h>
+
+BLYNK_WRITE(V0) {
+  int value = param.asInt();
+  digitalWrite(RELAY_1_PIN, value);
+}
+
+
+BLYNK_WRITE(V1) {
+  int value = param.asInt();
+  digitalWrite(RELAY_2_PIN, value);
+}
+
+BLYNK_CONNECTED() {
+  Blynk.virtualWrite(V0,0);
+  Blynk.virtualWrite(V1,0);
+}
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(RELAY_1_PIN,OUTPUT);
+  pinMode(RELAY_2_PIN,OUTPUT);
 
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  Blynk.run();
 }
